abba983af363658500a94c5e905e9ef166d1bd60
Szymon Kokot <szymon.kokot@illucrum.com>
2025-11-24 19:40:56 +0100

    Class rename

--- src/main/java/com/illucrum/tools/jhcr/watcher/JHCRListener.java
+++ src/main/java/com/illucrum/tools/jhcr/watcher/JHCRListener.java
@@ -6,7 +6,7 @@
  * JHCR is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * JHCR is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with JHCR. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
@@ -20,31 +20,37 @@ import java.nio.file.Paths;
 
 import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
 
-import com.illucrum.tools.jhcr.JHCRThread;
 import com.illucrum.tools.jhcr.logger.JHCRLogger;
 
-public class JHCRAlterationListener extends FileAlterationListenerAdaptor
+/**
+ * This class is responsible of keeping track of what files are being changed. When it detects a file change, it triggers the class redefinition or override.
+ * 
+ * @author Szymon Kokot
+ */
+public class JHCRListener extends FileAlterationListenerAdaptor
 {
     private final String fileExtension;
 
-    public JHCRAlterationListener(String fileExtension) {
+    public JHCRListener (String fileExtension)
+    {
         this.fileExtension = fileExtension;
     }
 
     @Override
     public void onFileCreate (File file)
     {
-        JHCRLogger.finest("File created...");
         String fileName = file.getName();
-        Path path = Paths.get(file.getAbsolutePath());
-        JHCRLogger.finer("Reading: " + file.getAbsolutePath());
 
         if (fileName.endsWith(this.fileExtension))
         {
+            JHCRLogger.finest("File created...");
+            Path path = Paths.get(file.getAbsolutePath());
+            JHCRLogger.finer("Reading: " + file.getAbsolutePath());
+
             try
             {
                 byte[] compiled = Files.readAllBytes(path);
-                JHCRThread.overrideClass(compiled);
+                JHCROverrider.overrideClass(compiled);
             }
             catch (Exception e)
             {
@@ -57,17 +63,17 @@ public class JHCRAlterationListener extends FileAlterationListenerAdaptor
     @Override
     public void onFileChange (File file)
     {
-        JHCRLogger.finest("File modified...");
         String fileName = file.getName();
-        Path path = Paths.get(file.getAbsolutePath());
-        JHCRLogger.finer("Reading: " + file.getAbsolutePath());
-
         if (fileName.endsWith(this.fileExtension))
         {
+            JHCRLogger.finest("File modified...");
+            Path path = Paths.get(file.getAbsolutePath());
+            JHCRLogger.finer("Reading: " + file.getAbsolutePath());
+
             try
             {
                 byte[] compiled = Files.readAllBytes(path);
-                JHCRThread.overrideClass(compiled);
+                JHCROverrider.overrideClass(compiled);
             }
             catch (Exception e)
             {
